4b2678b510e39111970713fd355525a45b2da2dc
ggggraceful <109974940+ggggraceful@users.noreply.github.com>
2023-02-18 12:33:11 +0900

    [Bronze I] Title: 팰린드롬수, Time: 120 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		while (true) {
+			String s = br.readLine();
+
+			if (s.equals("0")) { break; }
+
+			String reverse = new StringBuilder(s).reverse().toString();
+
+			if (s.equals(reverse)) {
+				sb.append("yes").append("\n");
+			} else {
+				sb.append("no").append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+}

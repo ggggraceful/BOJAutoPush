40d86c85fa7dae5abc6ec136ac3e28d7fe9ff4fd
ggggraceful <109974940+ggggraceful@users.noreply.github.com>
2023-03-27 12:55:57 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 748 ms, Memory: 51904 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -5,27 +5,32 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine());
+		HashSet<String> set = new HashSet<>();
 
-		HashMap<String, String> map = new HashMap<>();
-		ArrayList<String> arr = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			String s = br.readLine();
+			StringTokenizer st = new StringTokenizer(s, " ");
+			String name = st.nextToken();
+			String state = st.nextToken();
 
-		while (N-- > 0) {
-			String[] member = br.readLine().split(" ");
-			map.put(member[0], member[1]);
-		}
-
-		for (String key : map.keySet()) {
-			if (map.get(key).equals("enter")) {
-				arr.add(key);
+			if (state.equals("enter")) {
+				set.add(name);
+			}else if (state.equals("leave")) {
+				set.remove(name);
 			}
 		}
+		ArrayList<String> list = new ArrayList(set);
+		Collections.sort(list);
 
-		Collections.sort(arr);
-
-		for (int i = arr.size()-1; i >= 0; i--) {
-			System.out.println(arr.get(i));
+		for (int i = list.size()-1; i >= 0; i--) {
+			bw.write(list.get(i) + "\n");
 		}
+
+		br.close();
+		bw.flush();
+		bw.close();
 	}
 }

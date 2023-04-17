acfc3b64ddb39ecdd81b89a54f6eb7a94811c63d
ggggraceful <109974940+ggggraceful@users.noreply.github.com>
2023-04-18 06:02:44 +0900

    [level 0] Title: 두 수의 합, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120802. 두 수의 합/두 수의 합.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        
+        // 두수의 합
+        answer = num1 + num2;
+        
+        return answer;
+    }
+}

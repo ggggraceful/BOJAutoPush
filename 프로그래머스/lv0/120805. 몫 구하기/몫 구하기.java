c5abac88608c9f7b8b2e3071a63e0cd7b03bc081
ggggraceful <109974940+ggggraceful@users.noreply.github.com>
2023-04-18 06:04:22 +0900

    [level 0] Title: 몫 구하기, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        
+        // 몫 구하기
+        answer = num1 / num2;
+        
+        return answer;
+    }
+}
